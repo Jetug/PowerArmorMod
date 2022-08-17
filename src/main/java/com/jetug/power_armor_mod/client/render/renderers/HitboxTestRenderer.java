bc6156bd8fb69a4b387ad619ec3cc4846db3b292
Jetug <ksergeev01@gmail.com>
2022-08-17 17:45:14 +0300

    HitboxTest 2

--- src/main/java/com/jetug/power_armor_mod/client/render/renderers/HitboxTestRenderer.java
+++ src/main/java/com/jetug/power_armor_mod/client/render/renderers/HitboxTestRenderer.java
@@ -1,21 +1,34 @@
 package com.jetug.power_armor_mod.client.render.renderers;
 
 import com.jetug.power_armor_mod.PowerArmorMod;
+import com.jetug.power_armor_mod.client.model.ArmorModel;
+import com.jetug.power_armor_mod.client.model.PowerArmorModel;
 import com.jetug.power_armor_mod.client.model.TestModel;
+import com.jetug.power_armor_mod.client.render.ResourceHelper;
+import com.jetug.power_armor_mod.client.render.layers.ArmorPartLayer;
+import com.jetug.power_armor_mod.common.minecraft.entity.ArmorSlot;
+import com.jetug.power_armor_mod.common.minecraft.entity.HitboxTestEntity;
+import com.jetug.power_armor_mod.common.minecraft.entity.IPowerArmor;
+import com.jetug.power_armor_mod.common.minecraft.entity.PowerArmorEntity;
+import com.jetug.power_armor_mod.common.util.enums.BodyPart;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.LivingRenderer;
+import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.geo.render.built.GeoBone;
+import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
-public class HitboxTestRenderer <Type extends LivingEntity> extends LivingRenderer<Type, TestModel<Type>> {
-    private static final ResourceLocation TEXTURE = new ResourceLocation(PowerArmorMod.MOD_ID, "textures/entities/test_model.png");
 
-    public HitboxTestRenderer(EntityRendererManager context) {
-        super(context, new TestModel<>(), 0.5f);
-    }
+public class HitboxTestRenderer extends GeoEntityRenderer<HitboxTestEntity> {
+    private final PowerArmorModel<HitboxTestEntity> powerArmorModel;
+    private final ArmorModel<PowerArmorEntity> armorModel = new ArmorModel<>();
 
-    @Override
-    public ResourceLocation getTextureLocation(Type pEntity) {
-        return TEXTURE;
+    public HitboxTestRenderer(EntityRendererManager renderManager) {
+        super(renderManager,  new PowerArmorModel<>());
+
+        powerArmorModel = (PowerArmorModel<HitboxTestEntity>)getGeoModelProvider();
     }
 }
