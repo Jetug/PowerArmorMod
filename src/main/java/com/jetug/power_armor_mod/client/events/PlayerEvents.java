2afeeec13e03b9177aab50bfb9c93a050d30cfb7
Jetug <ksergeev01@gmail.com>
2022-08-23 17:58:14 +0300

    keyBind test

--- src/main/java/com/jetug/power_armor_mod/client/events/PlayerEvents.java
+++ src/main/java/com/jetug/power_armor_mod/client/events/PlayerEvents.java
@@ -1,9 +1,16 @@
 package com.jetug.power_armor_mod.client.events;
 
+import com.jetug.power_armor_mod.common.minecraft.entity.PowerArmorEntity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -20,5 +27,27 @@ public class PlayerEvents
 //        if(isWearingPowerArmor(player)){
 //            player.setInvisible(true);
 //        }
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @SubscribeEvent()
+    public void onEvent(InputEvent.KeyInputEvent event)
+    {
+        int key = event.getKey();
+        PlayerEntity player = Minecraft.getInstance().player;
+        player.sendMessage(new StringTextComponent("" + key), player.getUUID());
+
+        if(key == 57){
+
+            Entity entity = player.getVehicle();
+
+            if(player.isPassenger() && entity instanceof PowerArmorEntity){
+                ((PowerArmorEntity) entity).jump();
+            }
+
+        }
+
+        // Проверка, сработает при нажатии любой клавиши
+        System.out.println(key);
     }
 }
