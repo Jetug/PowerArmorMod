060bc53c128ce399715f7082f79d6494a368c1cf
Jetug <ksergeev01@gmail.com>
2022-12-09 14:22:53 +0300

    tttt

--- src/main/java/com/jetug/power_armor_mod/client/events/PlayerEvents.java
+++ src/main/java/com/jetug/power_armor_mod/client/events/PlayerEvents.java
@@ -1,5 +1,14 @@
 package com.jetug.power_armor_mod.client.events;
 
+import com.jetug.power_armor_mod.common.minecraft.entity.PowerArmorEntity;
+import com.jetug.power_armor_mod.common.minecraft.registery.ModEntityTypes;
+import com.mojang.blaze3d.platform.GlStateManager;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
@@ -9,6 +18,7 @@ import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.lwjgl.opengl.GL11;
 
 import static com.jetug.power_armor_mod.common.util.extensions.PlayerExtension.isWearingPowerArmor;
 
@@ -38,6 +48,56 @@ public class PlayerEvents
         }
     }
 
+
+    @OnlyIn(Dist.CLIENT)
+    @SubscribeEvent()
+    public static void onHandRender2(RenderPlayerEvent.Pre event)
+    {
+        event.setCanceled(true);
+        Minecraft mc = Minecraft.getInstance();
+
+        var ent = new PowerArmorEntity(EntityType.VEX, mc.level);
+
+        Minecraft.getInstance().getRenderManager().getRenderer(entity.getClass())
+                .doRender(entity, event.getX(), event.getY(), event.getZ(), event.getPlayer().rotationYaw, event.getPartialRenderTick());
+
+
+        Entity entity = EntityType.byKey(GodsCrownItem.getSelectedMorphMobOf(stack).toString()).get().create(event.getEntity().world);
+        Minecraft.getInstance().levelRenderer.getRenderManager()
+                .getRenderer(entity.getClass()).doRender(entity, event.getX(), event.getY(), event.getZ(), event.getPlayer().rotationYaw, event.getPartialRenderTick());
+
+
+        float x = 5.0F;
+        float y = 5.0F;
+        float z = 5.0F;
+        float width = 5.0F;
+        float height = 5.0F;
+
+        Minecraft mc = Minecraft.getInstance();
+
+        var renderManager = mc.levelRenderer;
+        LivingEntity player = mc.player;
+
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(-renderManager.viewerPosX + x, -renderManager.viewerPosY + y, -renderManager.viewerPosZ + z);
+        GlStateManager.rotate(System.currentTimeMillis() / 5 % 360, 0, 1, 0);
+
+        GlStateManager.disableTexture2D();
+        GlStateManager.enableBlend();
+        GlStateManager.disableCull();
+        GlStateManager.color(120.0F, 130.0F, 296.0F, 0.5F);
+        GL11.glBegin(GL11.GL_QUADS);
+        GL11.glVertex3f(0, 0, 0);
+        GL11.glVertex3f(0 + width, 0, 0);
+        GL11.glVertex3f(0 + width, 0 + height, 0);
+        GL11.glVertex3f(0, 0 + height, 0);
+        GL11.glEnd();
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
+        GlStateManager.enableCull();
+
+        GlStateManager.popMatrix();
+    }
 
     @OnlyIn(Dist.CLIENT)
     @SubscribeEvent()
