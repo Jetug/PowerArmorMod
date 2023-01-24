f42faefbd6c5dbd82d3b737bd163559983ed9d22
Jetug <ksergeev01@gmail.com>
2023-01-24 22:08:03 +0300

    Mojang api skin request

--- src/main/java/com/jetug/power_armor_mod/common/util/helpers/TextureHelper.java
+++ src/main/java/com/jetug/power_armor_mod/common/util/helpers/TextureHelper.java
@@ -2,7 +2,10 @@ package com.jetug.power_armor_mod.common.util.helpers;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.jetug.power_armor_mod.common.util.constants.Global;
+import com.mojang.authlib.minecraft.MinecraftProfileTexture;
+import com.mojang.authlib.minecraft.MinecraftSessionService;
 import com.mojang.blaze3d.platform.NativeImage;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.AbstractClientPlayer;
@@ -18,7 +21,9 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Map;
 import java.util.UUID;
 
 import static com.jetug.power_armor_mod.common.util.helpers.BufferedImageHelper.*;
@@ -51,51 +56,73 @@ public class TextureHelper {
 
     public static ResourceLocation getResourceLocation(AbstractClientPlayer clientPlayer) {
         var minecraft = Minecraft.getInstance();
-        var playerInfo = minecraft.getConnection().getPlayerInfo(clientPlayer.getUUID());
-        var gameProfile = playerInfo.getProfile();
-        var map = minecraft.getSkinManager().getInsecureSkinInformation(gameProfile);
 
-        skinRequest(clientPlayer.getUUID());
+        var uuid = clientPlayer.getUUID();
+        //uuid = UUID.fromString("494036be-71f6-4b58-bb8d-a483a18a322f");
+//        var playerInfo = minecraft.getConnection().getPlayerInfo(uuid);
+//        var gameProfile = playerInfo.getProfile();
+//        var map = minecraft.getSkinManager().getInsecureSkinInformation(gameProfile);
+
+        var skin = skinRequest(clientPlayer.getUUID());
 
 
-        if (map.containsKey(Type.SKIN)) {
-            final var skin = map.get(Type.SKIN);
-            try {
 
-                var url = new URL(skin.getUrl());
-                var image = ImageIO.read(url);
-                return createResource(image, clientPlayer.getUUID().toString());
+//        Map<Type, MinecraftProfileTexture> taxtures = null;
+//        try {
+//            var field = Minecraft.class.getDeclaredField("minecraftSessionService");
+//            field.setAccessible(true);
+//            var sess = (MinecraftSessionService)field.get(minecraft);
+//            taxtures = sess.getTextures(gameProfile, false);
+//
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
 
-            } catch (Exception e) {
-                //return clientPlayer.getSkinTextureLocation();
-                throw new RuntimeException(e);
-            }
-        }
+//        if (map.containsKey(Type.SKIN)) {
+//            final var skin = map.get(Type.SKIN);
+//            try {
+//
+//                var url = new URL(skin.getUrl());
+//                var image = ImageIO.read(url);
+//                return createResource(image, clientPlayer.getUUID().toString());
+//
+//            } catch (Exception e) {
+//                //return clientPlayer.getSkinTextureLocation();
+//                throw new RuntimeException(e);
+//            }
+//        }
         return null;
     }
 
 
-    private static void skinRequest(UUID uuid) {
+    private static BufferedImage skinRequest(UUID uuid) {
         var strUuid = uuid.toString();
         strUuid = strUuid.replace("-", "");
 
         var url = "https://sessionserver.mojang.com/session/minecraft/profile/" + strUuid;
-        url =     "https://sessionserver.mojang.com/session/minecraft/profile/494036be71f64b58bb8da483a18a322f";
-        var response = getHTML(url);
-        var gson = new Gson();
+        //url =     "https://sessionserver.mojang.com/session/minecraft/profile/494036be71f64b58bb8da483a18a322f";
 
-        User user = gson.fromJson(response, User.class);
-        var img = user.properties[0].value;
+        try {
+            var response = getHTML(url);
+            var gson = new Gson();
+            var user = gson.fromJson(response, User.class);
+            var encoded = user.properties[0].value;
+            var skinData = decodeBase64(encoded);
+            var itemsObject = JsonParser.parseString(skinData).getAsJsonObject();
+            var jsonUrl = itemsObject.get("textures").getAsJsonObject().get("SKIN").getAsJsonObject().get("url").getAsString();
+            var image = ImageIO.read(new URL(jsonUrl));
 
-        var image = base64ToBufferedImage(img);
-        File file = new File("C:\\Users\\Jetug\\Desktop\\result\\test_image.png");
-        file.createNewFile();
+            return image;
+//            File file = new File("C:\\Users\\Jetug\\Desktop\\result\\test_image.png");
+//            ImageIO.write(image, "png", file);
+        } catch (IOException e) {
+            return null;
+        }
     }
 
-    private static BufferedImage base64ToBufferedImage(String base64) {
-        var base = Base64.getDecoder();
-        var bytes = base.decode(base64);
-        return BufferedImageHelper.getImage(bytes);
+    private static String decodeBase64(String base64) {
+        byte[] decoded = Base64.getDecoder().decode(base64);
+        return new  String(decoded, StandardCharsets.UTF_8);
     }
 
     public static String getHTML(String urlToRead) {
@@ -106,7 +133,6 @@ public class TextureHelper {
             var conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             var res = conn.getResponseMessage();
-            //return res;
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
                 for (String line; (line = reader.readLine()) != null; ) {
                     result.append(line);
