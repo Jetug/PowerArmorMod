9fbc718d9d3428aa000d639d335533794f6736be
Jetug <ksergeev01@gmail.com>
2023-04-05 21:22:47 +0300

    merge

--- src/main/java/com/jetug/power_armor_mod/common/input/InputKey.java
+++ src/main/java/com/jetug/power_armor_mod/common/input/InputKey.java
@@ -1,12 +1,31 @@
 package com.jetug.power_armor_mod.common.input;
 
+import com.jetug.power_armor_mod.client.KeyBindings;
+import net.minecraft.client.KeyMapping;
+
+import static com.jetug.power_armor_mod.client.ClientConfig.OPTIONS;
+
 public enum InputKey {
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT,
-    JUMP,
-    LEAVE,
-    USE,
-    ATTACK
+    UP    (OPTIONS.keyUp),
+    DOWN  (OPTIONS.keyDown),
+    LEFT  (OPTIONS.keyLeft),
+    RIGHT (OPTIONS.keyRight),
+    JUMP  (OPTIONS.keyJump),
+    LEAVE (KeyBindings.LEAVE),
+    USE   (OPTIONS.keyUse),
+    ATTACK(OPTIONS.keyAttack);
+
+    public final KeyMapping key;
+
+    InputKey(KeyMapping key) {
+        this.key = key;
+    }
+
+    public static InputKey getByKey(KeyMapping key){
+        for (InputKey value : values()) {
+            if (value.key == key)
+                return value;
+        }
+        return null;
+    }
 }
