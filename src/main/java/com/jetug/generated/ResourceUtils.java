1289fe1d40451d649ade0061833161f800aa02e8
Jetug <ksergeev01@gmail.com>
2023-08-09 02:14:53 +0300

    Auto gen update

---
+++ src/main/java/com/jetug/generated/ResourceUtils.java
@@ -0,0 +1,9 @@
+package com.jetug.generated;
+
+import net.minecraft.resources.ResourceLocation;
+
+public class ResourceUtils {
+      public static ResourceLocation resourceLocation(String location){
+        return new ResourceLocation("power_armor_mod", location);
+    }
+}
--- src/main/java/com/jetug/generated/resources/Models.java
+++ src/main/java/com/jetug/generated/resources/Models.java
@@ -1,19 +1,22 @@
 package com.jetug.generated.resources;
 
+import net.minecraft.resources.ResourceLocation;
+import static com.jetug.generated.ResourceUtils.*;
+
 public class Models {
-    public static final String ARMOR_BODY = "/geo/armor/armor_body.geo.json";
-    public static final String ARMOR_HEAD = "/geo/armor/armor_head.geo.json";
-    public static final String ARMOR_LEFT_ARM = "/geo/armor/armor_left_arm.geo.json";
-    public static final String ARMOR_LEFT_LEG = "/geo/armor/armor_left_leg.geo.json";
-    public static final String ARMOR_RIGHT_ARM = "/geo/armor/armor_right_arm.geo.json";
-    public static final String ARMOR_RIGHT_LEG = "/geo/armor/armor_right_leg.geo.json";
-    public static final String ENGINE = "/geo/equipment/engine.geo.json";
-    public static final String JETPACK = "/geo/equipment/jetpack.geo.json";
-    public static final String POWER_ARMOR_FRAME = "/geo/frame/power_armor_frame.geo.json";
-    public static final String FRAME_BODY = "/geo/frame_parts/frame_body.geo.json";
-    public static final String FRAME_LEFT_ARM = "/geo/frame_parts/frame_left_arm.geo.json";
-    public static final String FRAME_LEFT_LEG = "/geo/frame_parts/frame_left_leg.geo.json";
-    public static final String FRAME_RIGHT_ARM = "/geo/frame_parts/frame_right_arm.geo.json";
-    public static final String FRAME_RIGHT_LEG = "/geo/frame_parts/frame_right_leg.geo.json";
-    public static final String HAND = "/geo/misc/hand.geo.json";
+    public static final ResourceLocation ARMOR_BODY = resourceLocation("/geo/armor/armor_body.geo.json");
+    public static final ResourceLocation ARMOR_HEAD = resourceLocation("/geo/armor/armor_head.geo.json");
+    public static final ResourceLocation ARMOR_LEFT_ARM = resourceLocation("/geo/armor/armor_left_arm.geo.json");
+    public static final ResourceLocation ARMOR_LEFT_LEG = resourceLocation("/geo/armor/armor_left_leg.geo.json");
+    public static final ResourceLocation ARMOR_RIGHT_ARM = resourceLocation("/geo/armor/armor_right_arm.geo.json");
+    public static final ResourceLocation ARMOR_RIGHT_LEG = resourceLocation("/geo/armor/armor_right_leg.geo.json");
+    public static final ResourceLocation ENGINE = resourceLocation("/geo/equipment/engine.geo.json");
+    public static final ResourceLocation JETPACK = resourceLocation("/geo/equipment/jetpack.geo.json");
+    public static final ResourceLocation POWER_ARMOR_FRAME = resourceLocation("/geo/frame/power_armor_frame.geo.json");
+    public static final ResourceLocation FRAME_BODY = resourceLocation("/geo/frame_parts/frame_body.geo.json");
+    public static final ResourceLocation FRAME_LEFT_ARM = resourceLocation("/geo/frame_parts/frame_left_arm.geo.json");
+    public static final ResourceLocation FRAME_LEFT_LEG = resourceLocation("/geo/frame_parts/frame_left_leg.geo.json");
+    public static final ResourceLocation FRAME_RIGHT_ARM = resourceLocation("/geo/frame_parts/frame_right_arm.geo.json");
+    public static final ResourceLocation FRAME_RIGHT_LEG = resourceLocation("/geo/frame_parts/frame_right_leg.geo.json");
+    public static final ResourceLocation HAND = resourceLocation("/geo/misc/hand.geo.json");
 }
