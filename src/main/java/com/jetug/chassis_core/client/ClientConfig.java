beab6fb7fb3fabff033a60f1bbcd68b80e1a00bb
Jetug <ksergeev01@gmail.com>
2023-09-10 16:41:12 +0300

    render test

--- src/main/java/com/jetug/chassis_core/client/ClientConfig.java
+++ src/main/java/com/jetug/chassis_core/client/ClientConfig.java
@@ -5,9 +5,11 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.Options;
 import net.minecraft.client.player.LocalPlayer;
 import org.jetbrains.annotations.NotNull;
+import software.bernie.geckolib3.file.GeoModelLoader;
 
 public class ClientConfig {
     public static final ModResourceManager modResourceManager = new ModResourceManager();
+    public static final GeoModelLoader modelLoader = new GeoModelLoader();
 
     @NotNull public static final Options OPTIONS = Minecraft.getInstance().options;
 
--- src/main/java/com/jetug/chassis_core/client/render/renderers/ChassisRenderer.java
+++ src/main/java/com/jetug/chassis_core/client/render/renderers/ChassisRenderer.java
@@ -1,9 +1,11 @@
 package com.jetug.chassis_core.client.render.renderers;
 
+import com.ibm.icu.impl.Pair;
 import com.jetug.chassis_core.client.model.*;
 import com.jetug.chassis_core.client.render.layers.*;
 import com.jetug.chassis_core.client.render.utils.GeoUtils;
-import com.jetug.chassis_core.common.data.enums.*;
+import com.jetug.chassis_core.common.data.json.EquipmentAttachment;
+import com.jetug.chassis_core.common.data.json.EquipmentConfig;
 import com.jetug.chassis_core.common.foundation.entity.*;
 import com.mojang.blaze3d.vertex.*;
 import com.mojang.math.Vector3f;
@@ -17,20 +19,24 @@ import net.minecraft.world.item.*;
 import org.jetbrains.annotations.*;
 import software.bernie.geckolib3.core.processor.*;
 import software.bernie.geckolib3.geo.render.built.*;
-import software.bernie.geckolib3.model.AnimatedGeoModel;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.jetug.chassis_core.client.render.utils.GeoUtils.*;
 import static com.jetug.chassis_core.common.data.constants.Bones.*;
+import static com.jetug.chassis_core.common.data.json.AttachmentMode.ADD;
+import static com.jetug.chassis_core.common.data.json.AttachmentMode.REPLACE;
 import static net.minecraft.world.entity.EquipmentSlot.MAINHAND;
 import static net.minecraft.world.entity.EquipmentSlot.OFFHAND;
 
 public class ChassisRenderer<T extends WearableChassis> extends ModGeoRenderer<T> {
-    protected final ChassisModel<T> armorChassisModel;
+    protected final ChassisModel<T> chassisModel;
     protected ItemStack mainHandItem, offHandItem;
 
     public ChassisRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new ChassisModel<>());
-        armorChassisModel = (ChassisModel<T>)getGeoModelProvider();
+        chassisModel = (ChassisModel<T>)getGeoModelProvider();
         initLayers();
     }
 
@@ -48,21 +54,30 @@ public class ChassisRenderer<T extends WearableChassis> extends ModGeoRenderer<T
 
         if (isInvisible(animatable)) return;
         for (var part : animatable.getEquipment())
-            renderEquipment(armorChassisModel ,animatable, part, false);
+            renderEquipment(chassisModel, animatable, part, false);
 
         super.render(model, animatable, partialTick, type, poseStack, bufferSource, buffer,
                 packedLight, packedOverlay, red, green, blue, alpha);
     }
 
+    @Override
+    public void renderRecursively(GeoBone bone, PoseStack poseStack, VertexConsumer buffer,
+                                  int packedLight, int packedOverlay,
+                                  float red, float green, float blue, float alpha) {
+        super.renderRecursively(bone, poseStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
 
-    public boolean isInvisible(T animatable) {
-        var clientPlayer = Minecraft.getInstance().player;
-        var pov = Minecraft.getInstance().options.getCameraType();
+        var value = attachmentForBone.get(bone.name);
+        if(value != null){
+            var chassisBone = getFrameBone(bone.name);
 
-        if (animatable.hasPassenger(clientPlayer) && pov == CameraType.FIRST_PERSON)
-            return true;
-        return false;
+            switch (value.second.mode) {
+                case ADD -> addBone(chassisModel, chassisBone, value.first);
+                case REPLACE -> replaceBone(chassisModel, chassisBone, value.first);
+            }
+        }
     }
+
+    private final Map<String, Pair<GeoBone, EquipmentAttachment>> attachmentForBone = new HashMap<>();
 
     @Override
     public void renderEarly(T animatable, PoseStack poseStack, float partialTick, MultiBufferSource bufferSource,
@@ -70,6 +85,17 @@ public class ChassisRenderer<T extends WearableChassis> extends ModGeoRenderer<T
         super.renderEarly(animatable, poseStack, partialTick, bufferSource, buffer, packedLight, packedOverlay, red, green, blue, partialTicks);
         mainHandItem = animatable.getPlayerItem(MAINHAND);
         offHandItem  = animatable.getPlayerItem(OFFHAND);
+
+        for (var part : animatable.getEquipment()) {
+            if(animatable.isEquipmentVisible(part)) {
+                var item = animatable.getEquipmentItem(part);
+                var config = item.getConfig();
+                for(var att : config.attachments){
+                    var equipmentBone = getArmorBone(config.getModelLocation(), att.armor);
+                    attachmentForBone.put(att.frame, Pair.of(equipmentBone, att));
+                }
+            }
+        }
     }
 
     @Nullable
@@ -108,11 +134,20 @@ public class ChassisRenderer<T extends WearableChassis> extends ModGeoRenderer<T
 
     @Nullable
     public GeoBone getFrameBone(String name){
-        return GeoUtils.getFrameBone(armorChassisModel, name);
+        return GeoUtils.getFrameBone(chassisModel, name);
     }
 
     @Nullable
     public GeoBone getArmorBone(ResourceLocation resourceLocation, String name){
         return GeoUtils.getArmorBone(resourceLocation, name);
+    }
+
+    private boolean isInvisible(T animatable) {
+        var clientPlayer = Minecraft.getInstance().player;
+        var pov = Minecraft.getInstance().options.getCameraType();
+
+        if (animatable.hasPassenger(clientPlayer) && pov == CameraType.FIRST_PERSON)
+            return true;
+        return false;
     }
 }
