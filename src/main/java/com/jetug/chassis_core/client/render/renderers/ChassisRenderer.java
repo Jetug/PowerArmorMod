d80dbc96a0a35345d963ff11e11d4337d2e6ceb3
Jetug <ksergeev01@gmail.com>
2023-09-13 07:21:27 +0300

    refactoring

--- src/main/java/com/jetug/chassis_core/client/render/renderers/ChassisRenderer.java
+++ src/main/java/com/jetug/chassis_core/client/render/renderers/ChassisRenderer.java
@@ -1,16 +1,10 @@
 package com.jetug.chassis_core.client.render.renderers;
 
-import com.ibm.icu.impl.Pair;
-import com.jetug.chassis_core.ChassisCore;
 import com.jetug.chassis_core.client.model.*;
 import com.jetug.chassis_core.client.render.layers.*;
 import com.jetug.chassis_core.client.render.utils.GeoUtils;
-import com.jetug.chassis_core.common.data.json.EquipmentAttachment;
 import com.jetug.chassis_core.common.foundation.entity.*;
-import com.jetug.chassis_core.common.foundation.item.ChassisEquipment;
 import com.mojang.blaze3d.vertex.*;
-import com.mojang.math.Matrix3f;
-import com.mojang.math.Matrix4f;
 import com.mojang.math.Vector3f;
 import net.minecraft.client.CameraType;
 import net.minecraft.client.Minecraft;
@@ -19,12 +13,10 @@ import net.minecraft.client.renderer.block.model.*;
 import net.minecraft.client.renderer.entity.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.*;
-import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.*;
 import software.bernie.geckolib3.core.processor.*;
 import software.bernie.geckolib3.geo.render.built.*;
 import software.bernie.geckolib3.util.EModelRenderCycle;
-import software.bernie.geckolib3.util.RenderUtils;
 
 import java.util.*;
 
@@ -32,19 +24,15 @@ import static com.jetug.chassis_core.common.data.constants.Bones.*;
 import static java.util.Collections.*;
 import static net.minecraft.world.entity.EquipmentSlot.MAINHAND;
 import static net.minecraft.world.entity.EquipmentSlot.OFFHAND;
-import static software.bernie.geckolib3.util.RenderUtils.*;
 
 public class ChassisRenderer<T extends WearableChassis> extends ModGeoRenderer<T> {
     protected final ChassisModel<T> chassisModel;
     protected ItemStack mainHandItem, offHandItem;
+    protected ArrayList<String> bonesToHide;
 
     public ChassisRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new ChassisModel<>());
         chassisModel = (ChassisModel<T>)getGeoModelProvider();
-        initLayers();
-    }
-
-    private void initLayers(){
         addLayer(new EquipmentLayer<>(this));
         addLayer(new PlayerHeadLayer<>(this));
     }
@@ -77,26 +65,12 @@ public class ChassisRenderer<T extends WearableChassis> extends ModGeoRenderer<T
         return result;
     }
 
-    @Nullable
-    protected ItemStack getItemForBone(String boneName, T animatable) {
-        var part = getChassisPart(boneName, animatable);
-        return part == null ? null : animatable.getEquipment(part);
-    }
-
-    @Nullable
-    protected String getChassisPart(String boneName, T animatable) {
-        return animatable.getConfig().getPartForBone(boneName);
-    }
-
     @Override
     public void renderRecursively(GeoBone bone, PoseStack poseStack, VertexConsumer buffer, int packedLight,
                                   int packedOverlay, float red, float green, float blue, float alpha) {
-        if (getCurrentModelRenderCycle() != EModelRenderCycle.INITIAL) {
+        if (getCurrentModelRenderCycle() != EModelRenderCycle.INITIAL)
             renderRecursivelyOriginal(bone, poseStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
-        }
-        else{
-            renderItemInHand(bone, poseStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
-        }
+        else renderItemInHand(bone, poseStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
     }
 
     private void renderRecursivelyOriginal(GeoBone bone, PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
@@ -124,8 +98,6 @@ public class ChassisRenderer<T extends WearableChassis> extends ModGeoRenderer<T
         poseStack.popPose();
     }
 
-    private ArrayList<String> bonesToHide;
-
     @Override
     public void renderEarly(T animatable, PoseStack poseStack, float partialTick, MultiBufferSource bufferSource,
                             VertexConsumer buffer, int packedLight, int packedOverlay, float red, float green, float blue, float partialTicks) {
@@ -134,9 +106,8 @@ public class ChassisRenderer<T extends WearableChassis> extends ModGeoRenderer<T
         offHandItem  = animatable.getPlayerItem(OFFHAND);
         bonesToHide = new ArrayList<>();
 
-        for (var config : animatable.getItemConfigs()) {
+        for (var config : animatable.getItemConfigs())
             addAll(bonesToHide, config.hide);
-        }
     }
 
     @Nullable
@@ -171,11 +142,6 @@ public class ChassisRenderer<T extends WearableChassis> extends ModGeoRenderer<T
             stack.translate(0, 0.125, 0.37);
             stack.mulPose(Vector3f.YP.rotationDegrees(180));
         }
-    }
-
-    @Nullable
-    public GeoBone getFrameBone(String name){
-        return GeoUtils.getFrameBone(chassisModel, name);
     }
 
     @Nullable
