36edd565e751915c8e2efbadb6943d84387fad1b
Jetug <ksergeev01@gmail.com>
2024-05-29 22:43:17 +0300

    bug fixes

--- src/main/java/com/jetug/chassis_core/client/events/PlayerEvents.java
+++ src/main/java/com/jetug/chassis_core/client/events/PlayerEvents.java
@@ -42,7 +42,7 @@ public class PlayerEvents {
                     event.getPackedLight());
         }
         poseStack.popPose();
-//        event.setCanceled(true);
+        event.setCanceled(true);
     }
 
 //    @OnlyIn(Dist.CLIENT)
