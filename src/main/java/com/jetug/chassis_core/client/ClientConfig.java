8da85b440657b061f42ba3e6610f30b915c275fd
Jetug <ksergeev01@gmail.com>
2024-05-30 23:11:13 +0300

    Refactoring

--- src/main/java/com/jetug/chassis_core/client/ClientConfig.java
+++ src/main/java/com/jetug/chassis_core/client/ClientConfig.java
@@ -1,6 +1,5 @@
 package com.jetug.chassis_core.client;
 
-import com.jetug.chassis_core.client.render.renderers.ChassisHeadRenderer;
 import com.jetug.chassis_core.client.resources.ModResourceManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.Options;
@@ -11,7 +10,6 @@ import org.jetbrains.annotations.NotNull;
 @OnlyIn(Dist.CLIENT)
 public class ClientConfig {
     public static final ModResourceManager modResourceManager = new ModResourceManager();
-    public static final ChassisHeadRenderer CHASSIS_HEAD_RENDERER = new ChassisHeadRenderer();
 
     @NotNull
     public static final Options OPTIONS = Minecraft.getInstance().options;
--- src/main/java/com/jetug/chassis_core/client/render/renderers/ChassisRenderer.java
+++ src/main/java/com/jetug/chassis_core/client/render/renderers/ChassisRenderer.java
@@ -1,6 +1,5 @@
 package com.jetug.chassis_core.client.render.renderers;
 
-import com.jetug.chassis_core.client.model.ChassisModel;
 import com.jetug.chassis_core.client.render.layers.EquipmentLayer;
 import com.jetug.chassis_core.client.render.layers.HeldItemLayer;
 import com.jetug.chassis_core.common.foundation.entity.WearableChassis;
@@ -11,20 +10,17 @@ import mod.azure.azurelib.cache.object.BakedGeoModel;
 import mod.azure.azurelib.cache.object.GeoBone;
 import mod.azure.azurelib.model.GeoModel;
 import mod.azure.azurelib.renderer.GeoEntityRenderer;
-import mod.azure.azurelib.util.ClientUtils;
 import mod.azure.azurelib.util.RenderUtils;
 import net.minecraft.client.CameraType;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.client.player.AbstractClientPlayer;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
-import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
@@ -71,12 +67,21 @@ public class ChassisRenderer<T extends WearableChassis> extends GeoEntityRendere
     public void renderRecursively(PoseStack poseStack, T animatable, GeoBone bone, RenderType renderType,
                                   MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick,
                                   int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-        var client = Minecraft.getInstance();
+        bone.setHidden(bonesToHide.contains(bone.getName()));
 
+        renderHead(poseStack, animatable, bone, packedLight, packedOverlay);
+
+        super.renderRecursively(poseStack, animatable, bone, renderType, bufferSource,
+                this.bufferSource.getBuffer(renderType), isReRender,
+                partialTick, packedLight, packedOverlay,
+                red, green, blue, alpha);
+    }
+
+    private void renderHead(PoseStack poseStack, T animatable, GeoBone bone, int packedLight, int packedOverlay) {
         if(!animatable.isInvisible() && Objects.equals(bone.getName(), "head") && animatable.hasPassenger()) {
             var passenger = animatable.getControllingPassenger();
 
-            if(client.getEntityRenderDispatcher().getRenderer(passenger) instanceof LivingEntityRenderer humanoidRenderer &&
+            if(getEntityRenderDispatcher().getRenderer(passenger) instanceof LivingEntityRenderer humanoidRenderer &&
                     humanoidRenderer.getModel() instanceof HumanoidModel humanoidModel) {
                 bone.setHidden(true);
                 bone.setChildrenHidden(false);
@@ -88,73 +93,16 @@ public class ChassisRenderer<T extends WearableChassis> extends GeoEntityRendere
                     poseStack.translate(0, -4, 0);
 
                     var skin = humanoidRenderer.getTextureLocation(passenger);
-                    var arm = this.bufferSource.getBuffer(RenderType.entitySolid(skin));
-                    var sleeve = this.bufferSource.getBuffer(RenderType.entityTranslucent(skin));
+                    var head = this.bufferSource.getBuffer(RenderType.entitySolid(skin));
+                    var hat = this.bufferSource.getBuffer(RenderType.entityTranslucent(skin));
 
-                    humanoidModel.head.setPos(bone.getPivotX(), bone.getPivotY(), bone.getPivotZ());
-                    humanoidModel.head.setRotation(0, 0, 0);
-                    humanoidModel.head.render(poseStack, arm, packedLight, packedOverlay, 1, 1, 1, 1);
-
-                    humanoidModel.hat.setPos(bone.getPivotX(), bone.getPivotY(), bone.getPivotZ());
-                    humanoidModel.hat.setRotation(0, 0, 0);
-                    humanoidModel.hat.render(poseStack, sleeve, packedLight, packedOverlay, 1, 1, 1, 1);
+                    renderHumanoidPart(poseStack, bone, packedLight, packedOverlay, humanoidModel, head);
+                    renderHumanoidPart(poseStack, bone, packedLight, packedOverlay, humanoidModel, hat);
                 }
                 poseStack.popPose();
             }
         }
-
-        super.renderRecursively(poseStack, animatable, bone, renderType, bufferSource,
-                this.bufferSource.getBuffer(renderType), isReRender,
-                partialTick, packedLight, packedOverlay,
-                red, green, blue, alpha);
     }
-
-//    @Override
-    public void renderRecursively2(PoseStack poseStack, T animatable, GeoBone bone, RenderType renderType,
-                                  MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender,
-                                  float partialTick, int packedLight, int packedOverlay,
-                                  float red, float green, float blue, float alpha) {
-        bone.setHidden(bonesToHide.contains(bone.getName()));
-
-        var client = Minecraft.getInstance();
-
-        if(animatable.hasPlayerPassenger() && Objects.equals(bone.getName(), "head")){
-            bone.setHidden(true);
-            bone.setChildrenHidden(false);
-
-            if(animatable.getPlayerPassenger() instanceof AbstractClientPlayer player) {
-                var playerRenderer = (PlayerRenderer) client.getEntityRenderDispatcher().getRenderer(player);
-                var playerModel = playerRenderer.getModel();
-
-                poseStack.pushPose();
-                {
-                    RenderUtils.prepMatrixForBone(poseStack, bone);
-                    poseStack.mulPose(Axis.ZP.rotationDegrees(180));
-//                poseStack.translate(X, -4 + Y, Z);
-                    poseStack.translate(0, -4, 0);
-
-                    var playerSkin = ((LocalPlayer) ClientUtils.getClientPlayer()).getSkinTextureLocation();
-                    var arm = this.bufferSource.getBuffer(RenderType.entitySolid(playerSkin));
-                    var sleeve = this.bufferSource.getBuffer(RenderType.entityTranslucent(playerSkin));
-
-                    playerModel.head.setPos(bone.getPivotX(), bone.getPivotY(), bone.getPivotZ());
-                    playerModel.head.setRotation(0, 0, 0);
-                    playerModel.head.render(poseStack, arm, packedLight, packedOverlay, 1, 1, 1, 1);
-
-                    playerModel.hat.setPos(bone.getPivotX(), bone.getPivotY(), bone.getPivotZ());
-                    playerModel.hat.setRotation(0, 0, 0);
-                    playerModel.hat.render(poseStack, sleeve, packedLight, packedOverlay, 1, 1, 1, 1);
-                }
-                poseStack.popPose();
-            }
-        }
-
-        super.renderRecursively(poseStack, animatable, bone, renderType, bufferSource,
-                this.bufferSource.getBuffer(renderType), isReRender,
-                partialTick, packedLight, packedOverlay,
-                red, green, blue, alpha);
-    }
-
     @Override
     public void renderChildBones(PoseStack poseStack, T animatable, GeoBone bone, RenderType renderType,
                                  MultiBufferSource bufferSource, VertexConsumer buffer,
@@ -193,37 +141,21 @@ public class ChassisRenderer<T extends WearableChassis> extends GeoEntityRendere
         };
     }
 
-//    protected Vector3d getWorldPos(GeoBone bone, PoseStack poseStack) {
-//        poseStack.pushPose();
-//        RenderUtils.translateMatrixToBone(poseStack, bone);
-//        RenderUtils.translateToPivotPoint(poseStack, bone);
-//
-//        boolean rotOverride = bone.getModelRotationMatrix() != null;
-//
-//        if (rotOverride) {
-//            poseStack.last().pose().mul(bone.getModelRotationMatrix());
-//            poseStack.last().normal().mul(new Matrix3f(bone.getModelRotationMatrix()));
-//        } else RenderUtils.rotateMatrixAroundBone(poseStack, bone);
-//
-//        RenderUtils.scaleMatrixForBone(poseStack, bone);
-//
-//        var poseState = (Matrix4f)poseStack.last().pose().clone();
-//        var localMatrix = RenderUtils.invertAndMultiplyMatrices(poseState, this.modelRenderTranslations);
-//        localMatrix.translate(new Vector3f(getRenderOffset(this.animatable, 1)));
-//        var worldState = localMatrix.clone();
-//        worldState.translate(new Vector3f(this.animatable.position()));
-//        var vec = new Vector4f(0, 0, 0, 1);
-//        vec.add(worldState);
-//        RenderUtils.translateAwayFromPivotPoint(poseStack, bone);
-//        poseStack.popPose();
-//
-//        return new Vector3d(vec.x(), vec.y(), vec.z());
-//    }
-
-    private boolean isInvisible(T animatable) {
+    protected boolean isInvisible(T animatable) {
         var clientPlayer = Minecraft.getInstance().player;
         var pov = Minecraft.getInstance().options.getCameraType();
 
         return animatable.hasPassenger(clientPlayer) && pov == CameraType.FIRST_PERSON;
+    }
+
+    @NotNull
+    private static EntityRenderDispatcher getEntityRenderDispatcher() {
+        return Minecraft.getInstance().getEntityRenderDispatcher();
+    }
+
+    private static void renderHumanoidPart(PoseStack poseStack, GeoBone bone, int packedLight, int packedOverlay, HumanoidModel humanoidModel, VertexConsumer head) {
+        humanoidModel.head.setPos(bone.getPivotX(), bone.getPivotY(), bone.getPivotZ());
+        humanoidModel.head.setRotation(0, 0, 0);
+        humanoidModel.head.render(poseStack, head, packedLight, packedOverlay, 1, 1, 1, 1);
     }
 }
