d5d508ff43d79aafe7f75ea2219dede9066f7b90
Jetug <ksergeev01@gmail.com>
2024-10-31 23:51:45 +0300

    input fix

--- src/main/java/com/jetug/chassis_core/client/utils/KeyUtils.java
+++ src/main/java/com/jetug/chassis_core/client/utils/KeyUtils.java
@@ -6,6 +6,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.util.Lazy;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static com.jetug.chassis_core.client.ClientConfig.OPTIONS;
@@ -13,16 +14,20 @@ import static com.jetug.chassis_core.client.ClientConfig.OPTIONS;
 @OnlyIn(Dist.CLIENT)
 public class KeyUtils {
     @OnlyIn(Dist.CLIENT)
-    public static Lazy<Map<Integer, InputKey>> keyMap = Lazy.of(() -> Map.of(
-            OPTIONS.keyUp.getKey().getValue(), InputKey.UP,
-            OPTIONS.keyDown.getKey().getValue(), InputKey.DOWN,
-            OPTIONS.keyLeft.getKey().getValue(), InputKey.LEFT,
-            OPTIONS.keyRight.getKey().getValue(), InputKey.RIGHT,
-            OPTIONS.keyJump.getKey().getValue(), InputKey.JUMP,
-            KeyBindings.LEAVE.getKey().getValue(), InputKey.LEAVE,
-            OPTIONS.keyUse.getKey().getValue(), InputKey.USE,
-            OPTIONS.keyAttack.getKey().getValue(), InputKey.ATTACK
-    ));
+    public static Lazy<Map<Integer, InputKey>> keyMap = Lazy.of(() -> {
+        var map = new HashMap<Integer, InputKey>();
+
+        map.put(OPTIONS.keyUp.getKey().getValue()       , InputKey.UP       );
+        map.put(OPTIONS.keyDown.getKey().getValue()     , InputKey.DOWN     );
+        map.put(OPTIONS.keyLeft.getKey().getValue()     , InputKey.LEFT     );
+        map.put(OPTIONS.keyRight.getKey().getValue()    , InputKey.RIGHT    );
+        map.put(OPTIONS.keyJump.getKey().getValue()     , InputKey.JUMP     );
+        map.put(KeyBindings.LEAVE.getKey().getValue()   , InputKey.LEAVE    );
+        map.put(OPTIONS.keyUse.getKey().getValue()      , InputKey.USE      );
+        map.put(OPTIONS.keyAttack.getKey().getValue()   , InputKey.ATTACK   );
+
+        return map;
+    });
 
     @OnlyIn(Dist.CLIENT)
     public static InputKey getByKey(int key) {
